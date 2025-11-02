d165629a20ec596c45290e96160cbac3fbac3262
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-02 16:19:36 +0530

    Js Practice

---
+++ Assignment 1/script.js
@@ -0,0 +1,60 @@
+// Hello c
+// console.log(5+4)
+
+// let age = 25;
+// let name = "stuti"
+// let isStudent = true;
+// let skills = ["js","HTML"];
+
+// let count = 5
+// console.log(count++)
+// console.log(++count)
+
+// count++ mein baad mein value add ho rahi hai count variable ke andar 
+
+// ++count mein pehle value add ho rahi hai count ke andar fir use ho rahi hai
+
+// let count = 5
+// console.log(count--)
+// console.log(--count)
+
+// vice versa of incriment
+
+// if(10>5 && 10<20 && 10==10)
+//     console.log("Haan bhai")
+// else
+//     console.log ("ni bhai")
+
+
+// console.log(a)
+// var a=10;
+
+// var dont show any error, it return  undefined
+
+// let b =10;
+// console.log(b);
+
+// let shows direct error creating temporal dead zone
+
+// test()
+// function test(){
+//     console.log("Hi")
+// }
+
+// function can run anywhere , it just need to call (just like - Berozgaar Dost or can also say always available for you person)
+
+// hello()
+// function hello(){
+//     console.log("Hi")
+// }
+
+// function pehle call karne pe bhi chal jata hai kyu ki vo global type hota hai ,usko kahi se bhi acces kiya ja sakta hai 
+
+// function is fully hoisted , Var variable is hoisted but not inicialization is hosted , let didt host
+
+// let age = prompt("Your age")
+// if (age>=18){
+//     console.log("Adult")
+// }
+// else
+//     console.log("Minor")
