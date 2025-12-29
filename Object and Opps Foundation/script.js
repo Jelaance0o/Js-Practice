c2f014af20fec542744c9eff0fc0a3e4c2ae1922
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-12-29 16:07:46 +0530

    Class and objects

---
+++ Object and Opps Foundation/script.js
@@ -0,0 +1,24 @@
+// Create a user object that stores name and email and has a login method which prints "User logged in."
+
+
+// let user = {
+//     name: "Jelaance",
+//     email: "jelaacne@gmail.com",
+//     login: function(){
+//         console.log("User logged in ")
+//     }   
+// }
+
+// console.log(user ) 
+// class User {
+//     constructor(name,email){
+//         this.name = name ;
+//         this.email = email;
+//     }
+//     logedin() {
+//         console.log("Loged in..")
+//     } //by this we shared the function in every new objects made otherwise if fucntion take 1kb per person then the programm can crash as scalabililty increase
+// } 
+// let user1 =new User("jelaance","jelaance@gmail.com")
+// let user2 =new User("Stuti","Stuti@gmail.com")
+
